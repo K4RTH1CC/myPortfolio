ba54f489f1a7f37f6745e3dfc57cf1c8d2f3661f
Karthik Nair <karthik16092002@gmail.com>
2023-04-12 03:13:49 +0530

    added target _blank to all external links

--- layouts/TopNavLayout.js
+++ layouts/TopNavLayout.js
@@ -11,9 +11,9 @@ function TopNavLayout(){
             <div className="bg-gray-500 p-4 flex justify-around">
                 <span className="text-gray-700 font-extrabold text-4xl font-header pl-10 pr-10 pt-4 pb-4">K4RTH1K</span>
                 <button className="hover:bg-slate-400 text-gray-700 font-extrabold text-xl mt-1 rounded-md bg-gray-400 pl-10 pr-10 pt-4 pb-4 border-b-4 border-gray-700 onClick={()=>router.push('/')}">Home</button>
-                <Link href="https://drive.google.com/file/d/1bWxKaFBVH05OtUGyWRFJSYLDDLkEniQK/view?usp=share_link" passHref={true}><button className="hover:bg-slate-400 text-gray-700 font-extrabold text-xl mt-1 rounded-md bg-gray-400 pl-10 pr-10 pt-4 pb-4 border-b-4 border-gray-700">Resume</button></Link>
-                <Link href="https://github.com/K4RTH1CC" passHref={true}><button className="hover:bg-slate-400 text-gray-700 font-extrabold text-xl mt-1 rounded-md bg-gray-400 pl-10 pr-10 pt-4 pb-4 border-b-4 border-gray-700">GitHub</button></Link>
-                <Link href="https://www.linkedin.com/in/karthik-nair16/" passHref={true}><button className="hover:bg-slate-400 text-gray-700 font-extrabold text-xl mt-1 rounded-md bg-gray-400 pl-10 pr-10 pt-4 pb-4 border-b-4 border-gray-700">LinkedIn</button></Link>
+                <Link href="https://drive.google.com/file/d/1bWxKaFBVH05OtUGyWRFJSYLDDLkEniQK/view?usp=share_link" passHref={true} target="_blank"><button className="hover:bg-slate-400 text-gray-700 font-extrabold text-xl mt-1 rounded-md bg-gray-400 pl-10 pr-10 pt-4 pb-4 border-b-4 border-gray-700">Resume</button></Link>
+                <Link href="https://github.com/K4RTH1CC" passHref={true} target="_blank"><button className="hover:bg-slate-400 text-gray-700 font-extrabold text-xl mt-1 rounded-md bg-gray-400 pl-10 pr-10 pt-4 pb-4 border-b-4 border-gray-700">GitHub</button></Link>
+                <Link href="https://www.linkedin.com/in/karthik-nair16/" passHref={true} target="_blank"><button className="hover:bg-slate-400 text-gray-700 font-extrabold text-xl mt-1 rounded-md bg-gray-400 pl-10 pr-10 pt-4 pb-4 border-b-4 border-gray-700">LinkedIn</button></Link>
             </div>
         </nav>
         </Fragment>
