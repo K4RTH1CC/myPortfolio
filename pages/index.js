2c1d36ef02a710a4821cf66d8ddbd3e1bda6337b
Karthik Nair <karthik16092002@gmail.com>
2023-04-11 13:42:18 +0530

    Added prototype carousel

--- pages/index.js
+++ pages/index.js
@@ -1,14 +1,14 @@
 import TopNavLayout from "@/layouts/TopNavLayout";
 import TypingAnimation from "@/layouts/TypingAnim";
+import ImgCarousel from "@/layouts/ImageCarousel";
 import { Fragment } from "react";
-
 
 function HomePage(){
     return <Fragment>
         <TopNavLayout />
         
         <div id="main" className="bg-slate-400 w-screen h-screen">
-            <div id="HOME" className="pt-40 pl-60">
+            <div id="HOME" className="pt-20 pl-60">
                 <br /><br /><br />
                 <span className="text-m ml-6 text-black">Hello, my name is</span>
                 <br />
@@ -16,8 +16,10 @@ function HomePage(){
                 <br /><br />
                 <span className="text-6xl ml-6 mt-12 font-bebas text-gray-700">Welcome to my</span>
                 <TypingAnimation words={['Website', 'Portfolio', 'Project Archive','Blog']} />
-
+                <br /><br />
             </div>
+            <div className="bg-slate-400"><ImgCarousel /></div>
+            
         </div>
     </Fragment>
    
